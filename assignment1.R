882b91e703ed5d9d7b81a24e1f3a80b5111fa599
Casey Dettlaff <casey.dettlaff@gmail.com>
2026-08-27 12:42:27 -0700

    Added projected skill profile

--- assignment1.R
+++ assignment1.R
@@ -23,3 +23,27 @@ print(
 
 ggsave('data_science_skill_profile.png')
 
+# Projected Profile
+cs <- 5
+math <- 6
+stat <- 4
+ml <- 5
+dom <- 7
+dvis <- 3
+com <- 2
+
+projected_skills <- data.frame(
+  skill = c("Computer Science", "Mathematics", "Statistics", "Machine Learning", "Domain Expertise", "Data Visualization", "Presentation"),
+  level = c(cs, math, stat, ml, dom, dvis, com))
+
+print(
+  ggplot(projected_skills, aes(x = skill, y = level)) +
+    geom_col() +
+    labs(
+      title = "Data Science Projected Skill Profile",
+      x = "Skill",
+      y = "Skill Level") +
+    theme_minimal())
+
+ggsave('data_science_projected_skill_profile.png')
+
